8814c0b3403bf2e0e1a541fcb478d3ec5775bd79
yavuzsel <yavuzsel@buffalo.edu>
2013-07-08 15:25:21 -0400

    add Constants.h

---
+++ StaffRoster/Constants.h
@@ -0,0 +1,25 @@
+//
+//  Constants.h
+//  StaffRoster
+//
+//  Created by yavuz on 6/13/13.
+//  Copyright (c) 2013 redhat. All rights reserved.
+//
+
+#ifndef StaffRoster_Constants_h
+#define StaffRoster_Constants_h
+
+#define kRESTfulBaseURL @"http://localhost/myrestapi/"
+
+#define kAGPushServerURL @"http://localhost/mypushserver"
+
+#define kMobileVariantID @"PUSH_VARIANT_ID"
+#define kMobileVariantSecret @"PUSH_VARIANT_SECRET"
+
+#define kAppTintColor [UIColor blackColor]
+
+#define kDefaultAppUserUID @"SOME_UID"
+
+#define kDefaultAppUserCN @"SOME_COMMON_NAME"
+
+#endif
